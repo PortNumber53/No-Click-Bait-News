5be76ef4e115da360c7ccb7a20612763a51cde64
Mauricio S Otta <mauricio@portnumber53.com>
2026-03-20 15:34:45 -0700

    Created boilerplate frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+import { cloudflare } from "@cloudflare/vite-plugin";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), cloudflare()],
+  server: {
+    port: 21010,
+    host: '0.0.0.0',
+    strictPort: true,
+  },
+})
